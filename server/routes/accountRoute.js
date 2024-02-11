94afb2aa9ca7cd268c26c8fb0d37bb3d964a4a43
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-02-11 19:25:27 +0300

    successfully adding an account

--- server/routes/accountRoute.js
+++ server/routes/accountRoute.js
@@ -1,4 +1,50 @@
 const express = require('express')
 const accountRouter = express.Router();
+const User = require('../models/User')
+
+// create account
+accountRouter.post('/add', async (req, res) => {
+    let userId = req.query.user
+
+    try {
+        let user = await User.findById(userId)
+        let account = {
+            name: req.body.name,
+            desc: req.body.desc,
+            currentAmount: req.body.currentAmount
+        }
+        let accounts = user.accounts
+        accounts.push(account)
+
+        await User.findByIdAndUpdate(userId, {
+            $set: {
+                accounts: accounts
+            }
+        })
+        let updatedUser = await User.findById(userId)
+
+        res.status(200)
+        res.json(updatedUser)
+    } catch (err) {
+        console.error(err)
+        res.status(500)
+        res.json({
+            status: "error",
+            message: "Account was not added"
+        })
+    }
+})
+
+// get all accounts
+accountRouter.get("/", (req, res) => {})
+
+// get a account
+accountRouter.get('/:id', (req, res) => {})
+
+// update a account
+accountRouter.put('/:id', (req, res) => {})
+
+// delete a account
+accountRouter.delete('/:id', (req, res) => {})
 
 module.exports = accountRouter
