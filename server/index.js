6cbe1875f80ebda123d9925604ab2a96b2df1505
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-02-11 14:48:07 +0300

    handling 404 errors

--- server/index.js
+++ server/index.js
@@ -13,8 +13,26 @@ dbConnection()
 
 const PORT = process.env.PORT
 
+// homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+// routers
+
+//  404 Errors
+app.all("*", (req,res) => {
+    res.status(404);
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, "views", "404.html"))
+    }else if(req.accepts('json')){
+        res.json({
+            status: "error",
+            mesage: "This resource could not be found"
+        })
+    }else if(req.accepts('txt')){
+        res.send("The resource could not be found")
+    }
 })
 
 app.listen(PORT, () => console.log(`We're running on port ${PORT}`))
