44a7c916077c43679bc8df577fa75a52d774b121
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-02-11 19:03:11 +0300

    successfully updating a user

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -5,8 +5,7 @@ const User = require('../models/User')
 
 //  create user
 userRouter.post('/add', async (req, res) => {
-    let salt = await bcrpyt.genSalt()
-    let hashedPassword = await bcrpyt.hash(req.body.password, salt)
+    let hashedPassword = await bcrpyt.hash(req.body.password, process.env.SALT)
     let userDetails = {
         username: req.body.username,
         email: req.body.email,
@@ -52,7 +51,41 @@ userRouter.get('/:id', async (req, res) => {
 })
 
 //  update user
-userRouter.put('/:id', (req, res) => {})
+userRouter.put('/:id', async (req, res) => {
+    let id = req.params.id
+    let updatedUser = {}
+    if(req.body.password) {
+        try {
+            let hashedPassword = await bcrpyt.hash(req.body.password, process.env.SALT)
+            updatedUser.password = hashedPassword
+        } catch (err) {
+            console.error(err)
+            res.status(500)
+            res.json({
+                status: "error",
+                message: "The user could not be updated"
+            })
+        }
+    }
+    if(req.body.profilePicture) updatedUser.profilePicture = req.body.profilePicture
+    if(req.body.currency) updatedUser.currency = req.body.currency
+
+    try {
+        await User.findByIdAndUpdate(id, {
+            $set: updatedUser
+        })
+        let user = await User.findById(id)
+        res.status(200)
+        res.json(user)
+    } catch (error) {
+        console.error(error)
+        res.status(500)
+        res.json({
+            status: "error",
+            message: "The user could not be updated"
+        })
+    }
+})
 
 // delete user
 userRouter.delete('/id', (req, res) => {})
