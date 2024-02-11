acbe5c5db6e19365c7a42beb8eaf4fd4a842a2f3
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-02-11 18:39:32 +0300

    successfully created a user

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,4 +1,45 @@
 const express = require('express')
 const userRouter = express.Router();
+const bcrpyt = require('bcryptjs')
+const User = require('../models/User')
+
+//  create user
+userRouter.post('/add', async (req, res) => {
+    let salt = await bcrpyt.genSalt()
+    let hashedPassword = await bcrpyt.hash(req.body.password, salt)
+    let userDetails = {
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword
+    }
+    try {
+        await User.create(userDetails)
+        res.status(201)
+        res.json(
+            {
+                status: 'success',
+                message: 'A new user has been created!'
+            }
+        )
+    } catch (error) {
+        console.error(error)
+        res.status(500)
+        res.json(
+            {
+                status: 'error',
+                message: 'User not created! Please try again'
+            }
+        )
+    }
+})
+
+// get user
+userRouter.get('/:id', (req, res) => {})
+
+//  update user
+userRouter.put('/:id', (req, res) => {})
+
+// delete user
+userRouter.delete('/id', (req, res) => {})
 
 module.exports = userRouter
