a733e271a3451367a2ed64315414832762dcc815
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-02-11 14:41:38 +0300

    loading static files + homepage

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+
+// loading static files such as css and images
+app.use(express.static(path.join(__dirname, 'public')))
 
 const dotenv = require('dotenv')
 dotenv.config({path: ".env"})
@@ -8,5 +12,9 @@ const dbConnection = require('./config/db')
 dbConnection()
 
 const PORT = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
 
 app.listen(PORT, () => console.log(`We're running on port ${PORT}`))
