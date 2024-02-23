ef4d1e7e0e351fddca8315c1ea42d4086546112c
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-02-23 22:38:12 +0300

    get all and individual expenses

--- server/routes/expenseRoute.js
+++ server/routes/expenseRoute.js
@@ -28,10 +28,48 @@ expenseRouter.post('/add', async(req, res) => {
 })
 
 // get all expenses
-expenseRouter.get("/", (req, res) => {})
+expenseRouter.get("/", async(req, res) => {
+    let userId = req.query.user
+
+    try {
+        let user = await User.findById(userId);
+
+        res.status(200);
+        res.json({
+            expenses: user.expenses
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500)
+        res.json({
+            status: "error",
+            message: "Expenses not found!"
+        })
+    }
+})
 
 // get a expense
-expenseRouter.get('/:id', (req, res) => {})
+expenseRouter.get('/:id', async(req, res) => {
+    let userId = req.query.user
+    let id = req.params.id
+
+    try {
+        let user = await User.findById(userId)
+        let expense = user.expenses.filter(expense => expense._id == id)
+
+        res.status(200)
+        res.json({
+            expense: expense
+        })
+    } catch (err) {
+        console.error(err);
+        res.status(500)
+        res.json({
+            status: "error",
+            message: "Saving not found!"
+        })
+    }
+})
 
 // update a expense
 expenseRouter.put('/:id', (req, res) => {})
