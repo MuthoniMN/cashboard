67365a07e8ab458d234443ad8a54c5511eeeb5bb
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-02-24 10:37:38 +0300

    creating the expense schema

---
+++ server/models/Expense.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const expenseSchema = new mongoose.Schema({
+    desc: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    account: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    }
+})
+
+module.exports = expenseSchema
