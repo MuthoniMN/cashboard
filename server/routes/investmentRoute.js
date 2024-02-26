a935c2a8f43f9e929c306d9fce18c1f7b561fb4a
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-02-26 14:09:19 +0300

    using investment controller

--- server/routes/investmentRoute.js
+++ server/routes/investmentRoute.js
@@ -1,119 +1,21 @@
 const express = require('express')
 const investmentRouter = express.Router();
-const User = require('../models/User')
+const User = require('../models/User');
+const investmentController = require('../controllers/investment');
 
 // create investment
-investmentRouter.post('/add', async (req, res) => {
-    let id = req.query.user
-    let newInvestment = {...req.body, lastModified: new Date()}
-    
-    try {
-        await User.findByIdAndUpdate(id, {
-            $push: {
-                investments: newInvestment
-            }
-        })
-        let user = await User.findById(id)
-        res.status(200)
-        res.json({
-            investments: user.investments
-        })
-    } catch (err) {
-        console.error(err)
-        res.status(500);
-        res.json({
-            status: "error",
-            message: "Investment could not be added!"
-        })
-    }
-})
+investmentRouter.post('/add', investmentController.addInvestment)
 
 // get all investments
-investmentRouter.get("/", async (req, res) => {
-    let id = req.query.user;
-    
-    try {
-        let user = await User.findById(id)
-
-        res.status(200)
-        res.json({investments:user.investments})
-    } catch (err) {
-        console.error(err)
-        res.status(404)
-        res.json({
-            status: 'error',
-            message: "User not found!"
-        })
-    }
-})
+investmentRouter.get("/", investmentController.getAllInvestments)
 
 // get a investment
-investmentRouter.get('/:id',  async(req, res) => {
-    let userId = req.query.user
-    let id = req.params.id
-
-    try {
-        let user = await User.findById(userId)
-        let investment = user.investments.filter(a => a._id == id)
-        res.status(200)
-        res.json(investment)
-    } catch (err) {
-        console.error(err)
-        res.status(500);
-        res.json({
-            status: "error",
-            message: "Could not find an investment" 
-        })
-    }
-})
+investmentRouter.get('/:id',  investmentController.getInvestment)
 
 // update a investment
-investmentRouter.put('/:id', async(req, res) => {
-    let userId = req.query.user;
-    let id = req.params.id;
-    let amount = req.body.amount;
-
-    try {
-        await User.updateOne({
-            _id: userId, 
-            "investments._id": id
-        }, {
-            $inc: {
-                "investments.$.currentAmount": amount
-            }
-        })
-
-        let user = await User.findById(userId);
-
-        res.status(200)
-        res.json({
-            investments: user.investments
-        })
-    } catch (error) {
-        console.error(err)
-        res.status(500)
-        res.json({
-            status: "error",
-            message: "Investment not updated."
-        })
-    }
-})
+investmentRouter.put('/:id', investmentController.updateInvestment)
 
 // delete a investment
-investmentRouter.delete('/:id', (req, res) => {
-    let userId = req.query.user;
-    let id = req.params.id;
-
-    try {
-        res.status(200)
-    } catch (error) {
-        console.error(err)
-        res.status(500)
-        res.json({
-            status: "error",
-            message: "Investment not deleted."
-        })
-    }
-})
+investmentRouter.delete('/:id', investmentController.deleteInvestment)
 
 module.exports = investmentRouter
