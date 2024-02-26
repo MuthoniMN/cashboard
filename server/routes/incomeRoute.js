f23d4224815d37926ac18f738030012e57bc7c6c
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-02-26 03:43:19 +0300

    using the income controller

--- server/routes/incomeRoute.js
+++ server/routes/incomeRoute.js
@@ -1,88 +1,15 @@
 const express = require('express')
 const incomeRouter = express.Router();
-const User = require('../models/User')
+const incomeController = require('../controllers/income');
 
 // create income
-incomeRouter.post('/add', async (req, res) => {
-    const id = req.query.user
-    const newIncome = req.body
-
-    try {
-        const user = await User.findById(id)
-
-        await User.findByIdAndUpdate(id, {
-            $push: {
-                income: newIncome
-            }
-        })
-
-        const updated = await User.findById(id)
-        res.status(201)
-        res.json({
-            income: updated.income
-        })
-    } catch (err) {
-        console.error(err)
-        res.status(500)
-        res.json({
-            status: "error",
-            message: "Income not added"
-        })
-    }
-
-})
+incomeRouter.post('/add', incomeController.addIncome)
 
 // get all income sources
-incomeRouter.get("/", async (req, res) => {
-    let id = req.query.user;
-    try {
-        let user = await User.findById(id)
-
-        res.status(200)
-        res.json({
-            income: user.income
-        })
-    } catch (err) {
-        console.error(err)
-        res.status(500)
-        res.json({
-            status: "error",
-            message: "Income not available"
-        })
-    }
-
-})
+incomeRouter.get("/", incomeController.getAllIncomes)
 
 // get a income
-incomeRouter.get('/:id', async (req, res) => {
-    let userId = req.query.user
-    let id = req.params.id
-
-    try {
-        let user = await User.findById(userId);
-        console.log(user)
-        let income = user.income;
-        let result
-
-        income.forEach(i => {
-            if(i._id == id){
-                result = i
-            }
-        })
-
-        res.status(200)
-        res.json({
-            income: result
-        })
-    } catch (err) {
-        console.error(err)
-        res.status(500)
-        res.json({
-            status: "error",
-            message: "Income not available"
-        })
-    }
-})
+incomeRouter.get('/:id', incomeController.getIncome)
 
 // update a income
 incomeRouter.put('/:id', (req, res) => {})
