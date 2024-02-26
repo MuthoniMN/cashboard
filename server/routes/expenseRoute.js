94d309231c936aa5a2cd3995a2f1e47031632f27
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-02-26 13:58:56 +0300

    using expense controller

--- server/routes/expenseRoute.js
+++ server/routes/expenseRoute.js
@@ -1,80 +1,18 @@
 const express = require('express')
 const expenseRouter = express.Router();
 const User = require('../models/User');
+const expenseController = require('../controllers/expense');
 
 // create expense
-expenseRouter.post('/add', async(req, res) => {
-    let id = req.query.user
-    let expense = {...req.body, date: new Date()};
-
-    try {
-        await User.findByIdAndUpdate(id, {
-            $push: {
-                expenses: expense
-            }
-        });
-        let updatedUser = await User.findById(id);
-
-        res.status(200)
-        res.json({expenses: updatedUser.expenses})
-    } catch (err) {
-        console.error(err);
-        res.status(500)
-        res.json({
-            status: "error",
-            message: "Expense not added"
-        })
-    }
-})
+expenseRouter.post('/add', expenseController.addExpense)
 
 // get all expenses
-expenseRouter.get("/", async(req, res) => {
-    let userId = req.query.user
-
-    try {
-        let user = await User.findById(userId);
-
-        res.status(200);
-        res.json({
-            expenses: user.expenses
-        })
-    } catch (error) {
-        console.error(error);
-        res.status(500)
-        res.json({
-            status: "error",
-            message: "Expenses not found!"
-        })
-    }
-})
+expenseRouter.get("/", expenseController.getAllExpenses)
 
 // get a expense
-expenseRouter.get('/:id', async(req, res) => {
-    let userId = req.query.user
-    let id = req.params.id
-
-    try {
-        let user = await User.findById(userId)
-        let expense = user.expenses.filter(expense => expense._id == id)
-
-        res.status(200)
-        res.json({
-            expense: expense
-        })
-    } catch (err) {
-        console.error(err);
-        res.status(500)
-        res.json({
-            status: "error",
-            message: "Saving not found!"
-        })
-    }
-})
-
-// update a expense
-expenseRouter.put('/:id', (req, res) => {})
+expenseRouter.get('/:id', expenseController.getExpense)
 
 // delete a expense
-expenseRouter.delete('/:id', (req, res) => {})
+expenseRouter.delete('/:id', expenseController.deleteExpense)
 
 module.exports = expenseRouter
