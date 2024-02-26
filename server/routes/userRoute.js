bd0973c82297896deae1ddf3f9bdfc4e15de91e0
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-02-26 14:52:22 +0300

    using user controller

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,110 +1,17 @@
 const express = require('express')
 const userRouter = express.Router();
-const bcrpyt = require('bcryptjs')
-const User = require('../models/User')
+const userController = require('../controllers/user');
 
 //  create user
-userRouter.post('/add', async (req, res) => {
-    let hashedPassword = await bcrpyt.hash(req.body.password, process.env.SALT)
-    let userDetails = {
-        username: req.body.username,
-        email: req.body.email,
-        password: hashedPassword
-    }
-    try {
-        await User.create(userDetails)
-        res.status(201)
-        res.json(
-            {
-                status: 'success',
-                message: 'A new user has been created!'
-            }
-        )
-    } catch (error) {
-        console.error(error)
-        res.status(500)
-        res.json(
-            {
-                status: 'error',
-                message: 'User not created! Please try again'
-            }
-        )
-    }
-})
+userRouter.post('/add', userController.addUser)
 
 // get user
-userRouter.get('/:id', async (req, res) => {
-    const id = req.params.id;
-    try {
-        let user = await User.findById(id)
-
-        res.status(200)
-        res.json(user)
-    } catch (error) {
-        console.error(error)
-        res.status(500)
-        res.json({
-            status: "error",
-            message: "We couldn't find the user. Please try again"
-        })
-    }
-})
+userRouter.get('/:id', userController.getUser)
 
 //  update user
-userRouter.put('/:id', async (req, res) => {
-    let id = req.params.id
-    let updatedUser = {}
-    if(req.body.password) {
-        try {
-            let hashedPassword = await bcrpyt.hash(req.body.password, process.env.SALT)
-            updatedUser.password = hashedPassword
-        } catch (err) {
-            console.error(err)
-            res.status(500)
-            res.json({
-                status: "error",
-                message: "The user could not be updated"
-            })
-        }
-    }
-    if(req.body.profilePicture) updatedUser.profilePicture = req.body.profilePicture
-    if(req.body.currency) updatedUser.currency = req.body.currency
-
-    try {
-        await User.findByIdAndUpdate(id, {
-            $set: updatedUser
-        })
-        let user = await User.findById(id)
-        res.status(200)
-        res.json(user)
-    } catch (error) {
-        console.error(error)
-        res.status(500)
-        res.json({
-            status: "error",
-            message: "The user could not be updated"
-        })
-    }
-})
+userRouter.put('/:id', userController.updateUser)
 
 // delete user
-userRouter.delete('/:id', async (req, res) => {
-    let id = req.params.id
-    try {
-        await User.findByIdAndDelete(id)
-        res.status(200)
-        res.json({
-            status: "success",
-            message: "The user was succesfully deleted"
-        })
-    } catch (error) {
-        console.error(error)
-        res.status(500)
-        res.json({
-            status: "error",
-            message: "The user could not be deleted!"
-        })
-    }
-})
+userRouter.delete('/:id', userController.deleteUser)
 
 module.exports = userRouter
