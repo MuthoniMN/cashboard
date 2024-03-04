8420aa0036b31414f74211905bb7337e6b5da82d
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-03-04 07:39:01 +0300

    linking pages

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,11 +1,16 @@
+import { Link } from 'react-router-dom';
 import './Nav.css'
 const Nav = () => {
     return (
         <nav>
             <img src="/logos/light-logo.png" alt="Cashboard App Logo" width={100} height={100} />
             <div>
-                <button className='secondary'>Sign In</button>
-                <button>Get Started</button>
+                <button className='secondary'>
+                    <Link to={'/login'}>Sign In</Link>
+                </button>
+                <button>
+                    <Link to={'/register'}>Get Started</Link>
+                </button>
             </div>
         </nav>
     )
