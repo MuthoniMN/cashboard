6f1457693e7bad57196325ede7d0f20610a962bc
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-03-05 21:46:24 +0300

    form validation

---
+++ client/src/utils/validate.js
@@ -0,0 +1,17 @@
+const validate = {};
+
+validate.isEmpty = function (...fields){
+    console.log(fields)
+    let emptyFields = fields.filter(field => field === undefined)
+
+    return emptyFields.length > 0 ? true : false
+}
+
+validate.validatePassword = function (password) {
+    const regex = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/);
+    const match = regex.test(password);
+    
+    return match;
+}
+
+export default validate;
