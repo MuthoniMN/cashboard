ee74e65cd24b6767d5eea2c3dd2c44fd6a6e3f10
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-03-05 21:50:32 +0300

    using auth context and protecting the dashboard route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { createBrowserRouter, RouterProvider, redirect } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Register from './pages/Register/Register';
+import Login from "./pages/Login/Login";
+import Dashboard from './pages/Dashboard/Dashboard'
+import { AuthContext,} from './contexts/AuthContext';
+import { useContext } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {token }= useContext(AuthContext)
+  const isAuthenticated = token !== null
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: isAuthenticated ? <Dashboard /> : <Home />
+    },
+    {
+      path: '/register',
+      element: <Register />
+    },
+    {
+      path: '/login',
+      element: <Login />
+    }
+  ])
 
   return (
-    <>
-      <h1>Cashboard App</h1>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
