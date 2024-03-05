de50c7249a544225466fa8a298dabcec9b5ffa05
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-03-05 21:49:26 +0300

    verifying password + bug fixes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,5 @@
-const generateToken =  require("../config/jwt");
+const bcrypt = require('bcryptjs')
+const { generateToken } =  require("../config/jwt");
 const User = require('../models/User');
 
 const authController = {};
@@ -6,16 +7,16 @@ const authController = {};
 authController.login = async function (req, res) {
     const {username, password } = req.body;
 
-    const user = User.findOne({username: username})
+    const user = await User.findOne({username: username})
 
-    if(user){
-        return res.status(404).mesage('User not found!');
+    if(!user){
+        return res.status(404).send('User not found!');
     }
 
-    let verified = User.verifyPassword(password)
+    let verified = await bcrypt.compare(password, user.password);
 
     if(!verified){
-        return res.status(400).message('Passwords don\'t match!')
+        return res.status(400).send('Passwords don\'t match!')
     }
 
     let token = generateToken(user)
