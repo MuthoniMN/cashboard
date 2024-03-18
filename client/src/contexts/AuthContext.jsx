c8ede5ef521ddc4e8afa2000eeea5fe5c0c5baef
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-03-19 02:42:45 +0300

    adding current user details

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -4,6 +4,7 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
+  const [currentUser, setCurrentUser] = useState(null);
   const [loading, setLoading] = useState(true); 
 
   useEffect(() => {
@@ -13,7 +14,7 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ token, setToken, loading }}>
+    <AuthContext.Provider value={{ token, setToken, currentUser, setCurrentUser, loading }}>
       {children}
     </AuthContext.Provider>
   );
