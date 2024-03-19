cd33fda1e6630c67f0f7307f40721d09e1a710a3
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-03-19 04:24:25 +0300

    refactor: using current user and successfully changing the currency

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import './Header.css';
-import { FaBell, FaUser } from "react-icons/fa";
+import { AuthContext } from "../../contexts/AuthContext";
+import axios from "axios";
 
 const Header = ({title, desc}) => {
+    const { currentUser, setCurrentUser} = useContext(AuthContext);
+    console.log(currentUser)
+    const [currencyTab, setCurrencyTab] = useState(currentUser.currency);
+    const [currency, setCurrency] = useState(currentUser.currency);
+    console.log(currencyTab)
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState('')
+
+    async function changeCurrency(e){
+        e.preventDefault();
+        setError("");
+        setSuccess("");
+        try {
+            const response = await axios.put(`http://localhost:5000/user/${currentUser._id}`, {currency: currency});
+            setCurrentUser(response.data)
+            setCurrencyTab(currentUser.currency)
+            setSuccess("Currency updated!")
+        } catch (err) {
+            console.error(err);
+            setError("Currency not updated!")
+        }
+    }
+    
     return (
         <header>
             <div>
@@ -10,17 +34,22 @@ const Header = ({title, desc}) => {
                 <p>{desc}</p>
             </div>
             <div>
-                <FaBell style={{ color: '#8F3985', fontSize: '24px' }} />
-                <div>
-                    <p className="avatar">
-                        <FaUser style={{ color: '#8F3985', fontSize: '24px' }} />
-                    </p>
-                    <div>
-                        <h3>username</h3>
-                        <p>User Account</p>
-                    </div>
-                </div>
+                <select value={currencyTab} onChange={(e) => setCurrencyTab(e.target.value)}>
+                    <option>{currentUser.currency}</option>
+                    <option>
+                        <button>Change Currency</button>
+                    </option>
+                </select>
+                <h3>@{currentUser.username}</h3>
             </div>
+            {currencyTab === "Change Currency" ? <div className="changeCurrency">
+                <form onSubmit={(e) => changeCurrency(e)}>
+                    <label htmlFor="currency">Currency: </label>
+                    <input type="text" value={currency} onChange={(e) => setCurrency(e.target.value)} />
+                    <input type="submit" value={"Change Currency"} />
+                </form>
+            </div> : ""}
+            <p className={success ? "toast toastSuccess" : error ? "toast toastError" : "toast"}>{success || error}</p>
         </header>
     )
 }
