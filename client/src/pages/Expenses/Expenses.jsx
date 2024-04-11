e689d962572bd987725e9ced61cf70c05537c554
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-04-11 14:45:38 +0300

    feat: expense page ui

--- client/src/pages/Expenses/Expenses.jsx
+++ client/src/pages/Expenses/Expenses.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import './Expenses.css'
+import React, { useContext, useState } from 'react';
+import {Link} from 'react-router-dom'
+import './Expenses.css';
 import Header from '../../components/Header/Header';
+import Categories from '../../components/Categories/Categories';
+import { AuthContext } from '../../contexts/AuthContext';
 
 const Expenses = () => {
+    const {currentUser} = useContext(AuthContext);
+    console.log(currentUser)
+    const [expenses, setExpenses] = useState(currentUser.expenses)
+    console.log(expenses)
+    const [categories, setCategories] = useState([]);
+    
     return (
         <>
         <Header title='Your Expenses' desc='An overview of your expenses.' />
-         <h1>Expenses</h1>
+        <section>
+            <h1>Expenses</h1>
+            <button>
+                <Link to='/expenses/add'>Add an Expense</Link>
+            </button>
+            <Categories heading="Where did your money go?" categories={categories} />
+        </section>
         </>
     )
 }
