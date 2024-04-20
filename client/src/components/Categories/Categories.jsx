69e0a40b1cef7ca9e0f42e3eaf628f3b60b17fb0
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-04-20 17:40:44 +0300

    refact: capitalizing first letter and adding percentages

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import "./Categories.css";
 
 export default function Categories({heading, categories}){
+    let totalAmount = categories.reduce((acc, current) => acc + current.total, 0)
+    console.log(totalAmount);
     return (
         <section className="categories">
             <h3>{heading}</h3>
             <section className="categoriesContainer">
                 {categories.map(category => (<div>
                     <div className="category">
-                        <h4>{category.name}</h4>
+                        <h4>{category.name.replace(category.name[0], category.name[0].toUpperCase())}</h4>
                         <p>{category.total}</p>
                     </div>
                     <div>
-                        <div style={{width: category.percent}} className="bar"></div>
+                        <div style={{width: `${(category.total / totalAmount) * 100}%`}} className="bar"></div>
                     </div>
                 </div>))}
             </section>
