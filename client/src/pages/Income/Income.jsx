4425c7e7a6fb7cd1569b819744ff49cc199dff73
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-04-20 20:56:57 +0300

    feat: successfully displaying income sources

--- client/src/pages/Income/Income.jsx
+++ client/src/pages/Income/Income.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Income.css'
 import Header from '../../components/Header/Header';
+import { AuthContext } from '../../contexts/AuthContext';
+import Card from '../../components/Card/Card';
+import { Link } from 'react-router-dom';
 
 const Income = () => {
+    const { currentUser, setCurrentUser } = useContext(AuthContext);
+    const income = currentUser.income
     return (
         <>
-        <Header title='Your Income Sources' desc='An overview of all your income sources' />
-         <h1>Income</h1>
+            <Header title='Your Income Sources' desc='An overview of all your income sources' />
+            <button>
+                <Link to='/income/add'>Add Income</Link>
+            </button>
+            <section className='incomeSection'>
+                {income.map(income => (
+                    <Card title={income.source} date={income.payDate} account={income.account} amount={income.amount} currency={income.currency} id={income._id} />
+                ))}
+            </section>
         </>
     )
 }
