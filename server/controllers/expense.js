9ec156a1af3a58fb0ae58478a72376f5a039163c
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-04-20 17:43:49 +0300

    refact: adding an expense, updating an account and adding the transaction

--- server/controllers/expense.js
+++ server/controllers/expense.js
@@ -3,14 +3,46 @@ const expenseController = {};
 
 expenseController.addExpense = async(req, res) => {
     let id = req.query.user
-    let expense = {...req.body, date: new Date()};
+    let expense = {...req.body};
 
     try {
+        // add the new expense
         await User.findByIdAndUpdate(id, {
             $push: {
                 expenses: expense
             }
         });
+
+        // get updated user
+        let user = await User.findById(id);
+
+        //get expense id
+        const expenseId = user.expenses[user.expenses.length - 1]._id
+
+        console.log(expense)
+        //update the account that paid for the expense
+        await User.updateOne({
+            _id: id,
+            "accounts._id": expense.account
+        }, {
+            $inc: {
+                "accounts.$.currentAmount": -expense.amount
+            }
+        })
+
+        // add a new transaction
+        await User.findByIdAndUpdate(id, {
+            $push: {
+                transactions: {
+                    type: 'expenses',
+                    typeId: expenseId,
+                    account: expense.account,
+                    timestamp: expense.date,
+                    amount: expense.amount
+                }
+            }
+        });
+
         let updatedUser = await User.findById(id);
 
         res.status(200)
@@ -72,9 +104,11 @@ expenseController.deleteExpense = async(req, res) => {
     let id = req.params.id;
 
     try {
-        let user = await User.findById(userId);
-
-        user.expenses.id(id).deleteOne()
+        await User.findOneAndUpdate(
+            { _id: userId },
+            { $pull: { expenses: { _id: id} } },
+            { new: true }
+        )
 
         res.status(200)
         res.json({
