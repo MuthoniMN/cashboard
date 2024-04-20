247c041b04272947dfc7213b932f379646b224a7
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-04-20 17:40:04 +0300

    feat: successfully adding expense form

--- client/src/components/AddExpense/AddExpense.jsx
+++ client/src/components/AddExpense/AddExpense.jsx
@@ -1,10 +1,45 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './AddExpense.css';
+import { AuthContext } from '../../contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import validate from '../../utils/validate';
+import axios from 'axios';
+import updateCurrentUser from '../../utils/updateUser';
 
 // groceries, lifestyle, bill, family & friends, 
-
 const AddExpenseForm = () => {
+    const {currentUser, setCurrentUser} = useContext(AuthContext);
+    const accounts = currentUser.accounts;
     const [expense, setExpense] = useState({});
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState('')
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+        setSuccess("");
+
+        if(validate.isEmpty(expense.desc, expense.amount, expense.account, expense.category, expense.date)){
+            setError("Please fill all the fields!")
+            return;
+        }
+{}
+        try {
+            const data = await axios.post(`http://localhost:5000/expense/add?user=${currentUser._id}`, {...expense, currency: currentUser.currency});
+            
+            setSuccess("Expense added successfully!")
+
+            let updated = await updateCurrentUser(currentUser._id);
+            setCurrentUser(updated);
+            navigate('/expenses')
+            
+        } catch (error) {
+            console.error(error);
+            setError("Please try again!!")
+        }
+    }
+
     return (
         <form className='dashboard'>
             <div>
@@ -12,8 +47,18 @@ const AddExpenseForm = () => {
                 <input type='text' id='desc' value={expense.desc} onChange={(e) => setExpense( {...expense, desc: e.target.value})} />
             </div>
             <div>
+                <label htmlFor='account'>Account: </label>
+                <select id='account' value={expense.account}  onChange={(e) => {console.log(e.target.value);setExpense( {...expense, account: e.target.value})}}>
+                    <option>Choose an Account</option>
+                    {accounts.map(account => (
+                        <option value={account._id}>{account.name}</option>
+                    ))}
+                </select>
+            </div>
+            <div>
                 <label htmlFor='category'>Category: </label>
-                <select type='text' id='category'value={expense.category} onChange={(e) => setExpense( {...expense, category: e.target.value})} >
+                <select id='category'value={expense.category} onChange={(e) => setExpense( {...expense, category: e.target.value})} >
+                    <option>Choose a Category</option>
                     <option value='groceries' >Groceries</option>
                     <option value='lifestyle'>Lifestyle</option>
                     <option value='bill'>Bill</option>
@@ -28,8 +73,9 @@ const AddExpenseForm = () => {
                 <label htmlFor='amount'>Amount: </label>
                 <input type='number' id='amount' value={expense.amount} onChange={(e) => setExpense( {...expense, amount: e.target.value})} />
             </div>
+            <p className={error ? 'error' : 'success'}>{error || success}</p>
 
-            <input type="submit" value="Add Expense" className='btn' />
+            <input type="submit" value="Add Expense" className='btn'  onClick={handleSubmit} />
         </form>
     )
 }
