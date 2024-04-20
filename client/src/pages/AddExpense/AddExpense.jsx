ccc96cb699906fc1daccc611efa3f951451bda0c
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-04-20 17:38:49 +0300

    feat: successfully adding an expense

--- client/src/pages/AddExpense/AddExpense.jsx
+++ client/src/pages/AddExpense/AddExpense.jsx
@@ -1,24 +1,13 @@
-import React, { useContext, useState } from 'react';
-import {Link} from 'react-router-dom'
+import React from 'react';
 import './AddExpense.css';
 import Header from '../../components/Header/Header';
-import { AuthContext } from '../../contexts/AuthContext';
 import AddExpenseForm from '../../components/AddExpense/AddExpense';
 
 const AddExpense = () => {
-    const {currentUser} = useContext(AuthContext);
-    const [expense, setExpense] = useState(currentUser.expenses);
-    
     return (
         <>
-        <Header title='Your Expenses' desc='An overview of your expenses.' />
+        <Header title='Add Expenses' desc='Add a new expense.' />
         <section>
-            <div style={{display: "flex", justifyContent:"space-between", alignItems: "center"}}>
-                <h1>Add Expenses</h1>
-                <button>
-                    <Link to='/expenses/add'>Back to Expense</Link>
-                </button>
-            </div>
             <AddExpenseForm />
         </section>
         </>
