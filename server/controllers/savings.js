feb118bac9662f47a11facf726bbbb760e12b0f4
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-04-22 18:01:55 +0300

    refact: modifying savings update

--- server/controllers/savings.js
+++ server/controllers/savings.js
@@ -1,11 +1,12 @@
 const User = require('../models/User');
 const savingsController = {};
 
-savingsController.addSavings = async(req, res) => {
+savingsController.addSavings = async (req, res) => {
     let id = req.query.user;
-    let saving = {...req.body, lastModified: new Date()}
+    let saving = { ...req.body, lastModified: new Date() }
 
     try {
+        // add saving
         await User.findByIdAndUpdate(id, {
             $push: {
                 savings: saving
@@ -15,7 +16,7 @@ savingsController.addSavings = async(req, res) => {
         let user = await User.findById(id)
 
         res.status(200)
-        res.json({savings: user.savings})
+        res.json({ savings: user.savings })
     } catch (err) {
         console.error(err)
         res.status(500)
@@ -26,14 +27,14 @@ savingsController.addSavings = async(req, res) => {
     }
 }
 
-savingsController.getAllSavings = async(req, res) => {
+savingsController.getAllSavings = async (req, res) => {
     let userId = req.query.user;
-    
+
     try {
         let user = await User.findById(userId)
 
         res.status(200);
-        res.json({savings: user.savings})
+        res.json({ savings: user.savings })
     } catch (err) {
         console.error(err);
         res.status(500);
@@ -44,14 +45,14 @@ savingsController.getAllSavings = async(req, res) => {
     }
 }
 
-savingsController.getSaving = async(req, res) => {
+savingsController.getSaving = async (req, res) => {
     let userId = req.query.user
     let id = req.params.id
 
     try {
         let user = await User.findById(userId)
 
-        let saving = user.savings.id(id)  
+        let saving = user.savings.id(id)
 
         res.status(200)
         res.json({
@@ -67,20 +68,46 @@ savingsController.getSaving = async(req, res) => {
     }
 }
 
-savingsController.updateSaving = async(req, res) => {
+savingsController.updateSaving = async (req, res) => {
     let userId = req.query.user;
     let id = req.params.id;
-    let amount = req.body.amount
+    let {amount, account, date} = req.body;
 
     try {
         await User.updateOne({
-            _id: userId, 
+            _id: userId,
             "savings._id": id
         }, {
             $inc: {
                 "savings.$.currentAmount": amount
             }
         })
+
+        let newUser = User.findById(userId);
+        let savingsId = newUser.savings[newUser.savings.length - 1]._id;
+
+        //update the account that paid for the expense
+        await User.updateOne({
+            _id: id,
+            "accounts._id": expense.account
+        }, {
+            $inc: {
+                "accounts.$.currentAmount": -amount
+            }
+        })
+
+        // add a new transaction
+        await User.findByIdAndUpdate(id, {
+            $push: {
+                transactions: {
+                    type: 'savings',
+                    typeId: savingsId,
+                    account: account,
+                    timestamp: expense.date,
+                    amount: amount
+                }
+            }
+        });
 
         let user = await User.findById(userId);
 
@@ -98,7 +125,7 @@ savingsController.updateSaving = async(req, res) => {
     }
 }
 
-savingsController.deleteSaving = async(req, res) => {
+savingsController.deleteSaving = async (req, res) => {
     let userId = req.query.user
     let id = req.params.id
 
