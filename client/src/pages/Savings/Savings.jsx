ca88f62b0a206c247d379c926d6cf753432a4670
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-04-23 22:30:27 +0300

    refact: savings page

--- client/src/pages/Savings/Savings.jsx
+++ client/src/pages/Savings/Savings.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Savings.css'
 import Header from '../../components/Header/Header';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext';
+import SavingsCard from '../../components/SavingsCard/SavingsCard';
 
 const Savings = () => {
+    const { currentUser } = useContext(AuthContext);
+    const savings = currentUser.savings;
     return (
         <>
-        <Header title='Your Savings' desc='An overview of all your saving goals' />
-         <h1>Savings</h1>
+            <Header title='Your Savings' desc='An overview of all your saving goals' />
+            <button>
+                <Link to="/savings/add">Add New Saving</Link>
+            </button>
+            <section className="flex">
+                {
+                    savings.map(saving => (
+                        <SavingsCard saving={saving} />
+                    ))
+                }
+                {savings.length === 0 && (
+                    <h3>No savings Yet</h3>
+                )}
+            </section>
+
         </>
     )
 }
