31bffc953031f3967b1e6fb9f6eb5012e906592e
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-04-25 18:39:07 +0300

    refact: separating component with functionality

--- client/src/components/EditSaving/EditSaving.jsx
+++ client/src/components/EditSaving/EditSaving.jsx
@@ -1,52 +1,20 @@
 import React, {useState, useContext} from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import validate from "../../utils/validate";
-import updateCurrentUser from "../../utils/updateUser";
 import { AuthContext } from "../../contexts/AuthContext";
 
-export default function EditSavingForm({id}){
-    const {currentUser, setCurrentUser} = useContext(AuthContext);
+export default function EditSavingForm({value, setValue, handleSubmit, error, success}){
+    const {currentUser} = useContext(AuthContext);
     const accounts = currentUser.accounts;
-    const [savings, setSavings] = useState({});
-    const [error, setError] = useState('')
-    const [success, setSuccess] = useState('')
-    const navigate = useNavigate()
-
-    const handleSubmit = async (e, id) => {
-        e.preventDefault();
-        setError("");
-        setSuccess("");
-
-        if(validate.isEmpty(savings.amount, savings.date, savings.account)){
-            setError("Please fill all the fields!")
-            return;
-        }
-{}
-        try {
-            const data = await axios.put(`http://localhost:5000/savings/${id}?user=${currentUser._id}`, {...savings});
-            
-            setSuccess("savings added successfully!")
-
-            let updated = await updateCurrentUser(currentUser._id);
-            setCurrentUser(updated);
-            navigate('/savings')
-            
-        } catch (error) {
-            console.error(error);
-            setError("Please try again!!")
-        }
-    }
 
     return (
         <form className='dashboard'>
             <div>
                 <label htmlFor='amount'>Deposit: </label>
-                <input type='number' id='amount' value={savings.amount} onChange={(e) => setSavings( {...savings, amount: e.target.value})} />
+                <input type='number' id='amount' value={value.amount} onChange={(e) => setValue( {...value, amount: e.target.value})} />
             </div>
             <div>
                 <label htmlFor='account'>Account: </label>
-                <select id='account' value={savings.account}  onChange={(e) => setSavings( {...savings, account: e.target.value})}>
+                <select id='account' value={value.account}  onChange={(e) => setValue( {...value, account: e.target.value})}>
                     <option>Choose an Account</option>
                     {accounts.map(account => (
                         <option value={account._id}>{account.name}</option>
@@ -55,11 +23,11 @@ export default function EditSavingForm({id}){
             </div>
             <div>
                 <label htmlFor='date'>Date: </label>
-                <input type='date' id='date' value={savings.date} onChange={(e) => setSavings( {...savings, date: e.target.value})} />
+                <input type='date' id='date' value={value.date} onChange={(e) => setValue( {...value, date: e.target.value})} />
             </div>
             <p className={error ? 'error' : 'success'}>{error || success}</p>
 
-            <input type="submit" value="Update Goal" className='btn'  onClick={(e) => handleSubmit(e, id)} />
+            <input type="submit" value="Update Goal" className='btn'  onClick={(e) => handleSubmit(e)} />
         </form>
     )
 }
--- client/src/pages/EditSaving/EditSaving.jsx
+++ client/src/pages/EditSaving/EditSaving.jsx
@@ -1,20 +1,54 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Header from "../../components/Header/Header";
 import { AuthContext } from "../../contexts/AuthContext";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import validate from "../../utils/validate";
+import updateCurrentUser from "../../utils/updateUser";
 import EditSavingForm from "../../components/EditSaving/EditSaving";
 
 export default function EditSaving() {
-    const { currentUser } = useContext(AuthContext);
+    const { currentUser, setCurrentUser } = useContext(AuthContext);
     const { id } = useParams();
     const saving = currentUser.savings.find(saving => saving._id === id);
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState('')
+    const navigate = useNavigate()
+    const [value, setValue] = useState(saving);
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+        setSuccess("");
+
+        if(validate.isEmpty(value.amount, value.date, value.account)){
+            setError("Please fill all the fields!")
+            return;
+        }
+{}
+        try {
+            const data = await axios.put(`http://localhost:5000/savings/${id}?user=${currentUser._id}`, {...value});
+            
+            setSuccess("Savings updated successfully!")
+
+            let updated = await updateCurrentUser(currentUser._id);
+            setCurrentUser(updated);
+            navigate('/savings')
+            
+        } catch (error) {
+            console.error(error);
+            setError("Please try again!!")
+        }
+    }
+
     return (
         <>
-            <Header title={'Edit Expense'} desc={'Deposit into a savings'} />
+            <Header title={'Edit Saving'} desc={'Deposit into a savings account'} />
             <section>
                 <h3>{saving.desc}</h3>
                 <p>Goal: {saving.goal}</p>
-                <EditSavingForm id={saving._id} />
+                <EditSavingForm value={value} setValue={setValue} handleSubmit={handleSubmit} error={error} success={success} />
             </section>
         </>
     )
