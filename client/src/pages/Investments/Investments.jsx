9e361fe63a7cc8a62696433284209ab4ac38614d
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-04-26 09:16:09 +0300

    refact: investments page

--- client/src/pages/Investments/Investments.jsx
+++ client/src/pages/Investments/Investments.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Investments.css'
 import Header from '../../components/Header/Header';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext';
+import InvestmentCard from '../../components/InvestmentCard/InvestmentCard';
 
 const Investments = () => {
+    const {currentUser} = useContext(AuthContext);
+    const investments = currentUser.investments;
+
     return (
         <>
         <Header title='Your Investments' desc='An overview of your investments' />
-         <h1>Investments</h1>
+            <button>
+                <Link to={'/investments/add'}>Add Investment</Link>
+            </button>
+            <section style={{padding: "24px"}}>
+                {investments.map(investment => (
+                    <InvestmentCard investment={investment} />
+                ))}
+            </section>
         </>
     )
 }
