a2b982c68b22cd8fe0ab04626c17c97288ec519b
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-04-29 14:51:56 +0300

    refact: adding environment variables

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,9 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
-      'process.env.REACT_CURRENCY_API_KEY': JSON.stringify(env.REACT_CURRENCY_API_KEY)
+      'process.env.REACT_CURRENCY_API_KEY': JSON.stringify(env.REACT_CURRENCY_API_KEY),
+      'process.env.REACT_APP_CURRENCY_API_URL': JSON.stringify(env.REACT_APP_CURRENCY_API_URL),
+      'process.env.REACT_APP_BACKEND_API': JSON.stringify(env.REACT_APP_BACKEND_API)
     },
     plugins: [react()],
   }
