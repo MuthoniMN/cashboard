f3c8f8421070df22a5fdece8f8a7741c229abe85
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-05-03 03:10:10 +0300

    logging url

--- client/src/utils/updateUser.js
+++ client/src/utils/updateUser.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export default async function updateCurrentUser(id){
+export default async function updateCurrentUser(id) {
+    console.log(process.env.REACT_APP_BACKEND_API);
     const res = await axios.get(`${process.env.REACT_APP_BACKEND_API}/user/${id}`);
 
     return res.data;
