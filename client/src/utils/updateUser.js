478b34accf260b66d045fce26cb984db61895f20
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-05-03 03:31:58 +0300

    refact: removing log

--- client/src/utils/updateUser.js
+++ client/src/utils/updateUser.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export default async function updateCurrentUser(id) {
-    console.log(process.env.REACT_APP_BACKEND_API);
     const res = await axios.get(`https://cashboard-f5x2.onrender.com/user/${id}`);
 
     return res.data;
